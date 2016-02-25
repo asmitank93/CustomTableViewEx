73345c81eaebe10bbe6da472693ae9f53f2663f8
Tops <tops@Topss-iMac.local>
2016-02-25 11:51:24 +0530

    Initial Commit

---
+++ CustomTableViewEx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomTableViewEx
+//
+//  Created by Tops on 2/25/16.
+//  Copyright (c) 2016 Tops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
